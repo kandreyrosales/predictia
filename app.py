96a2c153447b17ed8dd3f420c308ed1639db5466
Kevin <kandreyrosales@gmail.com>
2024-02-22 19:01:12 -0500

    changes

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def token_required(f):
     return decorated_function
 
 @app.route('/metricas_error')
-# @token_required
+@token_required
 def metricas_error():
     json_result = lamdba_metrics()
     mape_avg = round(json_result.get("average_mape", 0), 2) if len(json_result) else 0
